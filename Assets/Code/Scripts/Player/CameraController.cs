619b949d29463e9a5d0ec62581c4a506ac3f19f6
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-11-19 23:48:00 +0100

    fsm and camera is a shit

--- Assets/Code/Scripts/Player/CameraController.cs
+++ Assets/Code/Scripts/Player/CameraController.cs
@@ -7,21 +7,21 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private InputActionReference lookInput;
-    public Transform LookTransform;
-    public float distance = 15.0f;
-    public float yawRotationalSpeed = 720.0f;
-    public float pitchRotationalSpeed = 160.0f;
+    [SerializeField] private Transform lookTransform;
+    [SerializeField] private float distance = 15.0f;
+    [SerializeField] private float yawRotationalSpeed = 720.0f;
+    [SerializeField] private float pitchRotationalSpeed = 160.0f;
 
-    float pitch = 0.0f;
+    private float pitch = 0.0f;
 
-    public float minPitch = 30.0f;
-    public float maxPitch = -60.0f;
+    [SerializeField] private float minPitch = 30.0f;
+    [SerializeField] private float maxPitch = -60.0f;
 
-    public LayerMask avoidObjectsLayer;
-    public float avoidObjectsOffset;
+    [SerializeField] private LayerMask avoidObjectsLayer;
+    [SerializeField] private float avoidObjectsOffset;
 
-    public float minDistance;
-    public float maxDistance;
+    [SerializeField] private float minDistance;
+    [SerializeField] private float maxDistance;
     
     private bool cameraLocked;
     private Vector2 mouseInput;
@@ -34,30 +34,29 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
-        transform.LookAt(LookTransform.position);
-        float distance = Vector3.Distance(transform.position, LookTransform.position);
+        transform.LookAt(lookTransform.position);
+        var distance = Vector3.Distance(transform.position, lookTransform.position);
         distance = Mathf.Clamp(distance, minDistance, maxDistance);
-        Vector3 eulerAngels = transform.rotation.eulerAngles;
-        float yaw = eulerAngels.y;
+        var yaw = transform.rotation.eulerAngles.y;
 
         yaw += mouseInput.x * yawRotationalSpeed * Time.deltaTime;
         pitch += mouseInput.y * pitchRotationalSpeed * Time.deltaTime;
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
 
 
-        Vector3 l_ForwardCamera = new Vector3(Mathf.Sin(yaw * Mathf.Deg2Rad) * Mathf.Cos(pitch * Mathf.Deg2Rad), Mathf.Sin(pitch * Mathf.Deg2Rad),
-        Mathf.Cos(yaw * Mathf.Deg2Rad) * Mathf.Cos(pitch * Mathf.Deg2Rad));
-        //transform.position = LookTransform.position - l_ForwardCamera * distance;
-        //transform.LookAt(LookTransform.position);
-        Vector3 l_DesiredPosition = LookTransform.transform.position - l_ForwardCamera * distance;
+        var forwardCamera = new Vector3(Mathf.Sin(yaw * Mathf.Deg2Rad) * Mathf.Cos(pitch * Mathf.Deg2Rad), 
+                                        Mathf.Sin(pitch * Mathf.Deg2Rad),
+                                        Mathf.Cos(yaw * Mathf.Deg2Rad) * Mathf.Cos(pitch * Mathf.Deg2Rad));
+        
+        var desiredPosition = lookTransform.position - forwardCamera * distance;
 
-        Ray l_Ray = new Ray(LookTransform.position, -l_ForwardCamera);
+        var l_Ray = new Ray(lookTransform.position, -forwardCamera);
         RaycastHit l_RaycastHit;
         if (Physics.Raycast(l_Ray, out l_RaycastHit, distance, avoidObjectsLayer.value))
-            l_DesiredPosition = l_RaycastHit.point + l_ForwardCamera * avoidObjectsOffset;
+            desiredPosition = l_RaycastHit.point + forwardCamera * avoidObjectsOffset;
 
-        transform.position = l_DesiredPosition;
-        transform.LookAt(LookTransform.position);
+        transform.position = desiredPosition;
+        transform.LookAt(lookTransform.position);
     }
 #if UNITY_EDITOR
     public void LockMouse(InputAction.CallbackContext context)
