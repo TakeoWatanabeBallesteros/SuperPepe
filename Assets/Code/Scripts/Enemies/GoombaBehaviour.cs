716999f98b10a3a140d5281d6ca0b6c76b390ee0
Takeo Watanabe <takeowatanabe3@gmail.com>
2022-12-07 00:07:00 +0100

    ai

--- Assets/Code/Scripts/Enemies/GoombaBehaviour.cs
+++ Assets/Code/Scripts/Enemies/GoombaBehaviour.cs
@@ -2,32 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FSM;
-using Unity.Mathematics;
-using UnityEngine.AI;
-using UnityEngine.UI;
-using Random = UnityEngine.Random;
+using Pathfinding;
 
 public class GoombaBehaviour : MonoBehaviour
 {
-    [SerializeField] private CharacterController characterController;
+    [SerializeField] private IAstarAI agent;
     [SerializeField] private string currentState;
     [SerializeField] private Animator animator;
     [SerializeField] private Transform enemyEyes;
     [SerializeField] private Transform playerHead;
-    private NavMeshPath path;
-    [SerializeField] private List<Transform> m_PartolTargets;
-    private int m_CurrentPatrolTargetId = 0;
-    private int currentCornerId = 0;
+    [SerializeField] private List<Transform> patrolTargets;
+    private int currentPatrolTargetId = 0;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rangeDistance;
     [SerializeField] private float sightDistance;
     [SerializeField] private float maxChaseDistance;
     [SerializeField] private float sightAngle;
-    [SerializeField] private float rotationSpeed;
     [SerializeField] private LayerMask playerLayerMask;
     [SerializeField] private int maxHealth;
     [SerializeField] private float damage;
-    [SerializeField] private GameObject[] dropObjects;
     
     private float health;
     private StateMachine fsm;
@@ -47,8 +40,6 @@ public class GoombaBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        path = new NavMeshPath();
-        
         fsm = new StateMachine();
         
         fsm.AddState("Idle", new State(
@@ -56,37 +47,21 @@ public class GoombaBehaviour : MonoBehaviour
         fsm.AddState("Patrol", new State(
             onEnter: (state) =>
             {
-                currentCornerId = 0;
-                NavMesh.CalculatePath(transform.position, m_PartolTargets[m_CurrentPatrolTargetId].position,
-                    NavMesh.AllAreas, path);
+                
             },
             onLogic: (state) =>
             {
-                if (state.timer.Elapsed >= 3.0f)
-                {
-                    NavMesh.CalculatePath(transform.position, m_PartolTargets[m_CurrentPatrolTargetId].position,
-                        NavMesh.AllAreas, path);
-                    currentCornerId = 0;
-                    state.timer.Reset();
+                bool search = false;
+               
+                if (agent.reachedEndOfPath && !agent.pathPending) {
+                    currentPatrolTargetId = currentPatrolTargetId + 1;
+                    search = true;
                 }
- 
-                var aPos = transform.position;
-                aPos.y = 0;
-                var dPos = path.corners[currentCornerId];
-                dPos.y = 0;
-                
-                var direction = dPos - aPos;
-                Quaternion lookRotation = Quaternion.LookRotation(direction.normalized);
-                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
- 
-                characterController.Move(transform.forward.normalized * moveSpeed * Time.deltaTime 
-                                            + new Vector3(0, -7, 0) * Time.deltaTime);
-                
-                if(PathCornerPositionArrived())
-                    MoveToNextCornerPosition();
-                
-                if (PatrolTargetPositionArrived())
-                    MoveToNextPatrolPosition();
+
+                currentPatrolTargetId = currentPatrolTargetId % patrolTargets.Count;
+                agent.destination = patrolTargets[currentPatrolTargetId].position;
+
+                if (search) agent.SearchPath();
             },
             onExit: (state) =>
             {
@@ -96,39 +71,13 @@ public class GoombaBehaviour : MonoBehaviour
         fsm.AddState("Chase", new State(
             onEnter: (state) =>
             {
-                currentCornerId = 0;
-                NavMesh.CalculatePath(transform.position, GameManager.GetGameManager().GetPlayer().transform.position,
-                NavMesh.AllAreas, path);
+                
             },
             onLogic: (state) =>
             {
-                if (state.timer.Elapsed >= 1.0f)
-                {
-                    NavMesh.CalculatePath(transform.position,
-                        GameManager.GetGameManager().GetPlayer().transform.position,
-                        NavMesh.AllAreas, path);
-                    currentCornerId = 0;
-                    state.timer.Reset();
-                }
-                
-                for (int i = 0; i < path.corners.Length - 1; i++)
-                    Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
-
-                var aPos = transform.position;
-                aPos.y = 0;
-                if (currentCornerId >= path.corners.Length) currentCornerId = 0;
-                var dPos = path.corners[currentCornerId];
-                dPos.y = 0;
-                
-                var direction = dPos - aPos;
-                Quaternion lookRotation = Quaternion.LookRotation(direction.normalized);
-                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
- 
-                characterController.Move(transform.forward.normalized * moveSpeed * 2 * Time.deltaTime 
-                                         + new Vector3(0, -7, 0) * Time.deltaTime);
-                
-                if(PathCornerPositionArrived())
-                    MoveToNextCornerPosition();
+               if(state.timer.Elapsed < .3) return;
+               
+               agent.destination = GameManager.GetGameManager().GetPlayer().transform.position;
             },
             onExit: (state) =>
             {
@@ -193,41 +142,8 @@ public class GoombaBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerHead = Camera.main.transform;
         fsm.OnLogic();
-        if(fsm.ActiveState.name == "Attack")
-        {
-            transform.rotation = Quaternion.Euler(0.0f, enemyEyes.rotation.eulerAngles.y, 0.0f);
-            enemyEyes.LookAt(playerHead);
-        }
         currentState = fsm.ActiveState.name;
-    }
-    
-    private bool PathCornerPositionArrived()
-    {
-        return Vector3.Distance(transform.position, path.corners[currentCornerId]) < 0.3;
-    }
-    
-    private void MoveToNextCornerPosition()
-    {
-        ++currentCornerId;
-        if (currentCornerId < path.corners.Length) return;
-        currentCornerId = 0;
-        if(fsm.ActiveStateName == "Patrol") MoveToNextPatrolPosition();
-    }
-    
-    private bool PatrolTargetPositionArrived()
-    {
-        return Vector3.Distance(transform.position, m_PartolTargets[m_CurrentPatrolTargetId].position) < 0.3;
-    }
-    
-    private void MoveToNextPatrolPosition()
-    {
-        ++m_CurrentPatrolTargetId;
-        if (m_CurrentPatrolTargetId >= m_PartolTargets.Count)
-            m_CurrentPatrolTargetId = 0;
-        NavMesh.CalculatePath(transform.position, m_PartolTargets[m_CurrentPatrolTargetId].position,
-            NavMesh.AllAreas, path);
     }
 
     bool CanChase()
@@ -257,11 +173,6 @@ public class GoombaBehaviour : MonoBehaviour
         Ray l_Ray = new Ray(enemyEyes.position, l_Direction);
         return Vector3.Distance(PlayerPos, transform.position) < sightDistance && Vector3.Dot(l_ForwardXZ, l_DirectionPlayerXZ) > Mathf.Cos(sightAngle * Mathf.Deg2Rad / 2.0f) &&
                !Physics.Raycast(l_Ray, l_Lenght, playerLayerMask.value);
-    }
-    
-    private void RotateAtSpeed(float speed)
-    {
-        transform.Rotate(Vector3.up, rotationSpeed*Time.deltaTime);
     }
     
     private void MoveTowardsPlayer(float speed)
@@ -302,8 +213,7 @@ public class GoombaBehaviour : MonoBehaviour
             enemyEyes.transform.position += Vector3.down*Time.deltaTime*.70f/1f;
             yield return null;
         }
-
-        Instantiate(dropObjects[Random.Range(0, dropObjects.Length)], transform.position, quaternion.identity);
+        
         yield return new WaitForSeconds(3f);
         
         gameObject.SetActive(false);
